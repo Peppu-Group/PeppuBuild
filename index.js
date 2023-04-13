70e3cde64aa831f6624b49663bf38b4a2aac1b01
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-13 13:55:24 +0100

    saving this for later

--- index.js
+++ index.js
@@ -71,7 +71,47 @@ var editor = grapesjs.init({
         }]
     }
 });
-
-
-
+/* 
+const bm = editor.BlockManager;
+editor.on('load', () => {
+    editor.BlockManager.render([
+        bm.get('column1').set('category', ''),
+        bm.get('column2').set('category', ''),
+        bm.get('column3').set('category', ''),
+        bm.get('text').set('category', ''),
+        bm.get('image').set('category', '')
+    ])
+});
+*/
+editor.Panels.addPanel({
+    id: 'basic-actions',
+    el: '.panel__basic-actions',
+    buttons: [
+      {
+        id: 'visibility',
+        active: true, // active by default
+        className: 'btn-toggle-borders',
+        label: '<u>B</u>',
+        command: 'sw-visibility', // Built-in command
+      }, {
+        id: 'export',
+        className: 'btn-open-export',
+        label: 'Exp',
+        command: 'export-template',
+        context: 'export-template', // For grouping context of buttons from the same panel
+      }, {
+        id: 'show-json',
+        className: 'btn-show-json',
+        label: 'JSON',
+        context: 'show-json',
+        command(editor) {
+          editor.Modal.setTitle('Components JSON')
+            .setContent(`<textarea style="width:100%; height: 250px;">
+              ${JSON.stringify(editor.getComponents())}
+            </textarea>`)
+            .open();
+        },
+      }
+    ],
+  });
 editor.runCommand('sw-visibility');
