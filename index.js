b2e61fe508cd24613deb052c684889488d840957
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-13 14:21:05 +0100

    changed panel style background and font size

--- index.js
+++ index.js
@@ -1,117 +1,15 @@
-var editor = grapesjs.init({
-    container: '#gjs2',
-    height: '100%',
-    fromElement: true,
-    storageManager: { type: 0 },
-    plugins: ['gjs-blocks-basic'],
-    layerManager: {
-        appendTo: '#layers-container'
-    },
-    blockManager: {
-        appendTo: '#blocks'
-    },
-    styleManager: {
-        appendTo: '#style-manager-container',
-        sectors: [{
-            name: 'General',
-            open: false,
-            buildProps: ['float', 'display', 'position', 'top', 'right', 'left', 'bottom']
-        }, {
-            name: 'Dimension',
-            open: false,
-            buildProps: ['width', 'height', 'max-width', 'min-height', 'margin', 'padding']
-        }, {
-            name: 'Typography',
-            open: false,
-            buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align', 'text-shadow']
-        }, {
-            name: 'Decorations',
-            open: false,
-            buildProps: ['border-radius-c', 'background-color', 'border-radius', 'border', 'box-shadow', 'background'],
-        }, {
-            name: 'Extra',
-            open: false,
-            buildProps: ['opacity', 'transition', 'perspective', 'transform'],
-            properties: [{
-                type: 'slider',
-                property: 'opacity',
-                defaults: 1,
-                step: 0.01,
-                max: 1,
-                min: 0,
-            }]
-        }],
-    },
-    selectorManager: {
-        appendTo: '#selectors-container',
-    },
-    traitManager: {
-        appendTo: '#traits-container',
-    },
-    panels: {
-        defaults: [{
-            id: 'layers',
-            el: '#layers',
-            resizable: {
-                tc: 0,
-                cr: 1,
-                bc: 0,
-                keyWidth: 'flex-basis',
-            },
-        }, {
-            id: 'styles',
-            el: '#style-manager',
-            resizable: {
-                tc: 0,
-                cr: 0,
-                cl: 1,
-                bc: 0,
-                keyWidth: 'flex-basis',
-            },
-        }]
-    }
-});
-/* 
-const bm = editor.BlockManager;
-editor.on('load', () => {
-    editor.BlockManager.render([
-        bm.get('column1').set('category', ''),
-        bm.get('column2').set('category', ''),
-        bm.get('column3').set('category', ''),
-        bm.get('text').set('category', ''),
-        bm.get('image').set('category', '')
-    ])
-});
-*/
-editor.Panels.addPanel({
-    id: 'basic-actions',
-    el: '.panel__basic-actions',
-    buttons: [
-      {
-        id: 'visibility',
-        active: true, // active by default
-        className: 'btn-toggle-borders',
-        label: '<u>B</u>',
-        command: 'sw-visibility', // Built-in command
-      }, {
-        id: 'export',
-        className: 'btn-open-export',
-        label: 'Exp',
-        command: 'export-template',
-        context: 'export-template', // For grouping context of buttons from the same panel
-      }, {
-        id: 'show-json',
-        className: 'btn-show-json',
-        label: 'JSON',
-        context: 'show-json',
-        command(editor) {
-          editor.Modal.setTitle('Components JSON')
-            .setContent(`<textarea style="width:100%; height: 250px;">
-              ${JSON.stringify(editor.getComponents())}
-            </textarea>`)
-            .open();
-        },
+
+    var editor = grapesjs.init({
+      height: '100%',
+      showOffsets: true,
+      noticeOnUnload: true,
+      storageManager: true,
+      container: '#gjs',
+      fromElement: true,
+
+      plugins: ['grapesjs-preset-webpage', 'gjs-blocks-basic'],
+      pluginsOpts: {
+        'grapesjs-preset-webpage': {},
+        'gjs-blocks-basic': {}
       }
-    ],
-  });
-editor.runCommand('sw-visibility');
+    });
