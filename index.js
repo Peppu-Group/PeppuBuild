e67de1c414a83bf73370d711d04adef638c8d30b
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-13 14:42:11 +0100

    added export button

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
-    var editor = grapesjs.init({
-      height: '100%',
-      showOffsets: true,
-      noticeOnUnload: true,
-      storageManager: true,
-      container: '#gjs',
-      fromElement: true,
+var editor = grapesjs.init({
+    height: '100%',
+    showOffsets: true,
+    noticeOnUnload: true,
+    storageManager: true,
+    container: '#gjs',
+    fromElement: true,
 
-      plugins: ['grapesjs-preset-webpage', 'gjs-blocks-basic'],
-      pluginsOpts: {
+    plugins: ['grapesjs-preset-webpage', 'gjs-blocks-basic', 'grapesjs-plugin-export'],
+    pluginsOpts: {
         'grapesjs-preset-webpage': {},
-        'gjs-blocks-basic': {}
-      }
-    });
+        'gjs-blocks-basic': {},
+        'grapesjs-plugin-export': {}
+    }
+});
