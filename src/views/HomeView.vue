8c9fa284e1490473029d085be9bd1ed41e9105ea
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-22 20:08:03 +0100

    hero section ready

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,43 +3,43 @@
 </template>
 
 <script>
-  import grapesjs from 'grapesjs'
-  import 'grapesjs/dist/css/grapes.min.css'
-  import 'grapesjs/dist/grapes.min.js'
-  import 'grapesjs-preset-webpage/dist/index'
-  import plugin from 'grapesjs-preset-webpage'
-  import grapesjsblocks from 'grapesjs-blocks-basic'
-  import grapesnav from 'grapesjs-navbar'
-  import { auth } from '../netlify/redirect'
+import grapesjs from 'grapesjs'
+import 'grapesjs/dist/css/grapes.min.css'
+import 'grapesjs/dist/grapes.min.js'
+import 'grapesjs-preset-webpage/dist/index'
+import plugin from 'grapesjs-preset-webpage'
+import grapesjsblocks from 'grapesjs-blocks-basic'
+import grapesnav from 'grapesjs-navbar'
+import { auth } from '../netlify/redirect'
 
-  export default {
-      name: 'HomeView',
+export default {
+  name: 'HomeView',
 
-      mounted() {
-          var editor = grapesjs.init({
-              container: '#gjs',
-              height: '1000px',
-              width: '100%',
-              plugins: [plugin, grapesjsblocks, grapesnav],
-              storageManager: true,
-              pluginsOpts: {
-                  'grapesjs-preset-webpage': {},
-                  'grapesjs-blocks-basic': {},
-                  'grapesjs-navbar': {}
-              }
-          })
+  mounted() {
+    var editor = grapesjs.init({
+      container: '#gjs',
+      height: '1000px',
+      width: '100%',
+      plugins: [plugin,  grapesnav],
+      storageManager: true,
+      pluginsOpts: {
+        'grapesjs-preset-webpage': {},
+        'grapesjs-blocks-basic': {},
+        'grapesjs-navbar': {}
+      }
+    })
 
-          // create navbar component since inline can't contain js
-          editor.Components.addType('footer', {
-              // Make the editor understand when to bind `my-input-type`
-              isComponent: (el) => el.tagName === 'INPUT',
+    // create footer section
+    editor.Components.addType('footer', {
+      // Make the editor understand when to bind `my-input-type`
+      isComponent: (el) => el.tagName === 'INPUT',
 
-              // Model definition
-              model: {
-                  // Default properties
-                  defaults: {
-                      tagName: 'div',
-                      components: `
+      // Model definition
+      model: {
+        // Default properties
+        defaults: {
+          tagName: 'div',
+          components: `
             <footer>
         <div class="footer-content">
             <h3>Foolish Developer</h3>
@@ -66,7 +66,7 @@
 
     </footer>
       `,
-      styles: `
+          styles: `
       footer{
     position: absolute;
     bottom: 0;
@@ -75,7 +75,6 @@
     background: #111;
     height: auto;
     width: 100vw;
-
     padding-top: 40px;
     color: #fff;
 }
@@ -180,41 +179,118 @@ display: block;
   display: flex;
   margin-top: 10px;
   margin-bottom: 20px;
-}
+}`,
+        }
+      }
+    })
 
-      `,
-                  }
-              }
-          })
+    editor.Panels.addButton('devices-c', [
+      {
+        id: 'save',
+        className: 'button',
+        label: 'Publish Website',
+        command: publishWebsite
+      }
+    ])
+    const bm = editor.BlockManager
+    // A block for the custom component
+    bm.add('footer', {
+      label: 'Footer',
+      content: { type: 'footer' },
+      attributes: {
+        class: 'fa fa-sign-out'
+      },
+      category: 'Footer'
+    })
 
-          editor.Panels.addButton('devices-c', [
-              {
-                  id: 'save',
-                  className: 'button',
-                  label: 'Publish Website',
-                  command: publishWebsite
-              }
-          ])
-          const bm = editor.BlockManager
-          // A block for the custom component
-          bm.add('footer', {
-              label: 'Footer',
-              content: { type: 'footer' },
-              attributes: {
-                  class: 'fa fa-sign-out'
-              },
-              category: 'Footer'
-          })
-          bm.add('social-sign-in-butttons-section', {
-              label: 'Button',
-              /*
-              content: [
-                { type: 'image' },
-                `<div>Extra</div>`
-              ],
-              */
-              content: [
-                  `<div class="topnav" id="myTopnav">
+      // create hero section
+      editor.Components.addType('hero', {
+      // Make the editor understand when to bind `my-input-type`
+
+      // Model definition
+      model: {
+        // Default properties
+        defaults: {
+          tagName: 'hero',
+          components: `
+          <div class="container">
+            <div class="text">
+              <div class="big">Hello, my name is Ali</div>
+              <div class="small">Welcome to my website</div>
+            </div>
+            <div class="buttons">
+              <button type="button" class="sub-button">Get Started</button>
+              <button type="button" class="sub-button">Learn More</button>
+            </div>
+          </div>
+          `,
+          styles: `
+          .container {
+            height: auto;
+            width: 100vw;
+            background-color: #111;
+          }
+
+          .text {
+            color: white;
+            text-align: center;
+          }
+
+          .text .big{
+            font-size: 50px;
+            padding: 20px
+          }
+
+          .text .small{
+            font-size: 25px;
+            padding: 5px;
+          }
+
+          .buttons {
+            text-align: center;
+          }
+
+          .sub-button {
+            padding: 14px 28px;
+            font-size: 16px;
+            border: 2px solid black;
+            background-color: black;
+            cursor: pointer;
+            border-color: #04AA6D;
+            color: green;
+            border-radius: 5px;
+            margin: 10px 10px 30px 10px;
+          }
+
+          .sub-button:hover {
+            background-color: #04AA6D;
+            color: white;
+          }
+          `,
+        }
+      }
+    })
+
+    // A block for the custom component
+    bm.add('hero', {
+      label: 'Hero',
+      content: { type: 'hero' },
+      attributes: {
+        class: 'fa fa-envelope-open'
+      },
+      category: 'Blocks'
+    })
+    
+    bm.add('social-sign-in-butttons-section', {
+      label: 'Button',
+      /*
+      content: [
+        { type: 'image' },
+        `<div>Extra</div>`
+      ],
+      */
+      content: [
+        `<div class="topnav" id="myTopnav">
           <a href="#">
             <img class='img' src="https://i.ibb.co/8BCBPZK/IMG-2437-1.png"></img>
             </a>
@@ -274,96 +350,97 @@ display: block;
   }
     </style>
     `
-              ],
-              traits: [{ type: 'image', src: `https://i.ibb.co/8BCBPZK/IMG-2437-1.png` }],
-              attributes: {
-                  class: 'fa fa-square'
-              },
-              category: 'Basic'
-          })
-          bm.remove('link-block')
-          bm.remove('quote')
+      ],
+      traits: [{ type: 'image', src: `https://i.ibb.co/8BCBPZK/IMG-2437-1.png` }],
+      attributes: {
+        class: 'fa fa-square'
+      },
+      category: 'Basic'
+    })
+    
+    bm.remove('link-block')
+    bm.remove('quote')
 
-          async function publishWebsite() {
-              /*
-              netlifyIdentity.open('login');
-              let item =  window.localStorage.getItem('gotrue.user');
-              let accessToken = JSON.parse(item);
-              let client = new NetlifyAPI(accessToken.token.access_token)
-              let sites = await client.listSites();
-              console.log(sites);
-              */
-              /* 
-               netlifyIdentity.open("login");
-               let item = window.localStorage.getItem("gotrue.user");
-               let accessToken = JSON.parse(item);
-               // let client = new NetlifyAPI(accessToken.token.access_token)
-               // Headers object
-               let headers = { "Content-Type": "application/json", "Authorization": `Bearer ${accessToken.token.access_token}` };
-               // Body
-               let body = { "name": "cherry" };
-               // URL
-               let url = "https://api.netlify.com/api/v1/accounts";
-               // Request
-               let results = await axios.get(url, { headers });
-               // Showing results
-               console.log(results);
-               */
-              return auth.call()
-          }
-      }
+    async function publishWebsite() {
+      /*
+      netlifyIdentity.open('login');
+      let item =  window.localStorage.getItem('gotrue.user');
+      let accessToken = JSON.parse(item);
+      let client = new NetlifyAPI(accessToken.token.access_token)
+      let sites = await client.listSites();
+      console.log(sites);
+      */
+      /* 
+       netlifyIdentity.open("login");
+       let item = window.localStorage.getItem("gotrue.user");
+       let accessToken = JSON.parse(item);
+       // let client = new NetlifyAPI(accessToken.token.access_token)
+       // Headers object
+       let headers = { "Content-Type": "application/json", "Authorization": `Bearer ${accessToken.token.access_token}` };
+       // Body
+       let body = { "name": "cherry" };
+       // URL
+       let url = "https://api.netlify.com/api/v1/accounts";
+       // Request
+       let results = await axios.get(url, { headers });
+       // Showing results
+       console.log(results);
+       */
+      return auth.call()
+    }
   }
+}
 </script>
 <style>
-  body,
-  html {
-      margin: 0;
-      font: caption;
-      height: 100%;
-  }
+body,
+html {
+  margin: 0;
+  font: caption;
+  height: 100%;
+}
 
-  /* Theming */
-  .gjs-one-bg {
-      background-color: black !important;
-  }
+/* Theming */
+.gjs-one-bg {
+  background-color: black !important;
+}
 
-  .gjs-two-color {
-      color: white !important;
-  }
+.gjs-two-color {
+  color: white !important;
+}
 
-  .gjs-three-bg {
-      background-color: #4caf50 !important;
-      color: white !important;
-  }
+.gjs-three-bg {
+  background-color: #4caf50 !important;
+  color: white !important;
+}
 
-  .gjs-four-color,
-  .gjs-four-color-h:hover {
-      color: #4caf50 !important;
-  }
+.gjs-four-color,
+.gjs-four-color-h:hover {
+  color: #4caf50 !important;
+}
 
-  /* Style manager properties*/
-  .gjs-sm-properties {
-      font-size: 14px;
-  }
+/* Style manager properties*/
+.gjs-sm-properties {
+  font-size: 14px;
+}
 
-  .gjs-pn-panel {
-      font-size: 15px;
-  }
+.gjs-pn-panel {
+  font-size: 15px;
+}
 
-  .gjs-block {
-      font-size: 1px;
-  }
+.gjs-block {
+  font-size: 1px;
+}
 
-  .button,
-  .button:hover {
-      background-color: white;
-      /* Green */
-      border: none;
-      color: #4caf50;
-      padding: 5px 30px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-  }
+.button,
+.button:hover {
+  background-color: white;
+  /* Green */
+  border: none;
+  color: #4caf50;
+  padding: 5px 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
 </style>
