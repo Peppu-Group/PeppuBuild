a8d9900acf5590f8425dcd959e7a1e4fd8747ecd
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-27 15:48:52 +0100

    added html and css for landing page

---
+++ src/views/Home.vue
@@ -0,0 +1,312 @@
+<template>
+    <body id="iljx">
+        <div class="navbar">
+            <div class="navbar-container" data-gjs="navbar">
+                <a href="/" class="navbar-brand"></a>
+                <div class="navbar-burger" id="iksl">
+                    <div class="navbar-burger-line">
+                    </div>
+                    <div class="navbar-burger-line">
+                    </div>
+                    <div class="navbar-burger-line">
+                    </div>
+                </div>
+                <div data-gjs="navbar-items" class="navbar-items-c">
+                    <nav class="navbar-menu">
+                        <a class="navbar-menu-link">Home</a>
+                        <a class="navbar-menu-link">About</a>
+                        <a class="navbar-menu-link">Contact</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <hero>
+            <div class="contain">
+                <div class="text">
+                    <div class="big">Create a Landing Page
+                        <br />in
+                        <br />Five Minutes
+                        <br />
+                    </div>
+                    <div class="small">Drag and drop blocks to build your landing page faster with PeppuDraw. 
+                    </div>
+                </div>
+                <div class="buttons">
+                    <button type="button" class="sub-button">Get Started</button>
+                    <button type="button" class="sub-button">Learn More</button>
+                </div>
+            </div>
+        </hero>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div id="icm3j" class="gjs-row">
+        </div>
+        <section-two>
+            <div class="container">
+                <div class="child">
+                    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImZpbGw6IHJnYmEoMCwwLDAsMC4xNSk7IHRyYW5zZm9ybTogc2NhbGUoMC43NSkiPgogICAgICAgIDxwYXRoIGQ9Ik04LjUgMTMuNWwyLjUgMyAzLjUtNC41IDQuNSA2SDVtMTYgMVY1YTIgMiAwIDAgMC0yLTJINWMtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMnoiPjwvcGF0aD4KICAgICAgPC9zdmc+"
+                        class="img" />
+                </div>
+                <p id="i2933d">
+                <div id="izoy9i">
+                    <span id="ielerm">
+                        <font face="verdana, sans-serif" id="iisrmk">PeppuDraw is an easy-to-use website builder that allows
+                            users to create beautiful and functional websites with just a few clicks. With its intuitive
+                            drag-and-drop interface and a wide range of customizable templates, PeppuDraw makes it easy for
+                            users to create a website that perfectly suits their needs.
+                        </font>
+                    </span>
+                </div>
+                <div id="ipnywg">
+                    <span id="ifm9hj">
+                        <font face="verdana, sans-serif">
+                            <br />
+                        </font>
+                    </span>
+                </div>
+                <div id="ixgy7n">
+                    <span id="i41p2l">
+                        <font face="verdana, sans-serif" id="iukymc">Whether you're building a personal blog or a business
+                            website, PeppuDraw has everything you need to create a professional-looking website in minutes.
+                            Don't just take our word for it, watch the video to see how easy it is to create a website with
+                            PeppuDraw
+                        </font>
+                    </span>
+                </div>
+                </p>
+            </div>
+        </section-two>
+        <section class="bdg-sect">
+            <div>
+            </div>
+        </section>
+        <div>
+        </div>
+        <div id="ij48m" class="gjs-row">
+        </div>
+        <section-two>
+            <h1 class="center">Style Your Page
+            </h1>
+            <div class="container">
+                <div class="child">
+                    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImZpbGw6IHJnYmEoMCwwLDAsMC4xNSk7IHRyYW5zZm9ybTogc2NhbGUoMC43NSkiPgogICAgICAgIDxwYXRoIGQ9Ik04LjUgMTMuNWwyLjUgMyAzLjUtNC41IDQuNSA2SDVtMTYgMVY1YTIgMiAwIDAgMC0yLTJINWMtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMnoiPjwvcGF0aD4KICAgICAgPC9zdmc+"
+                        class="img" />
+                </div>
+                <p id="irhzg">
+                    <span id="imkp8h">
+                        <font face="verdana, sans-serif" id="itckci">GrapesJs style manager is an excellent tool for web
+                            designers and developers who want to create stylish and professional websites without the need
+                            for extensive coding. With its intuitive interface, users can easily customize pre-built style
+                            components or create new ones from scratch.
+                        </font>
+                    </span>
+                <div id="ig8tto">
+                    <span id="iid36h">
+                        <font face="verdana, sans-serif">
+                            <br />
+                        </font>
+                    </span>
+                </div>
+                <div id="it6yxq">
+                    <span id="idln1s">
+                        <font face="verdana, sans-serif" id="i58w3m">The style manager allows users to control every aspect
+                            of their website's design, from fonts and colors to margins and padding, all with just a few
+                            clicks. GrapesJs style manager is flexible, powerful, and easy to use, making it a top choice
+                            for designers looking to create unique and visually stunning websites.
+                        </font>
+                    </span>
+                </div>
+                </p>
+            </div>
+        </section-two>
+        <h1 class="center">Features
+        </h1>
+        <cards>
+            <div class="cards">
+                <div class="card">
+                    <div class="card-header">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">Own your Code
+                        </div>
+                        <div class="card-sub-title">Download code files in one click
+                        </div>
+                        <div class="card-desc">This feature gives you more control over your website. You can download your
+                            code files or host them in Git repositories.
+                        </div>
+                    </div>
+                </div>
+                <cards>
+                    <div class="cards">
+                        <div class="card">
+                            <div class="card-header">
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title">Deploy with Netlify
+                                </div>
+                                <div class="card-sub-title">Publish your website with Netlify
+                                </div>
+                                <div class="card-desc">PeppuDraw, integrates with Netlify. You can easily publish your
+                                    website with few clicks and no prior knowledge.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </cards>
+                <cards>
+                    <div class="cards">
+                        <div class="card">
+                            <div class="card-header">
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title">Components
+                                </div>
+                                <div class="card-sub-title">Ready-to-use building blocks
+                                </div>
+                                <div class="card-desc">Components are ready-to-use building blocks to allow you create
+                                    websites quickly. Components can range from text boxes to complex features.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </cards>
+                <cards>
+                    <div class="cards">
+                        <div class="card">
+                            <div class="card-header">
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title">Ease of Use
+                                </div>
+                                <div class="card-sub-title">Style components with the style manager
+                                </div>
+                                <div class="card-desc">One of our standout feature is GrapesJs intuitive style manager,
+                                    which allows you to customise pre-built components.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </cards>
+            </div>
+            <section-one id="ifowug">
+                <h1 class="center-one">Get Started
+                </h1>
+                <div class="one-container">
+                    <p id="i5r9ef">
+                    <div id="i3r5tg">
+                        <span id="idabqv">
+                            <font face="verdana, sans-serif">Ready to start building your own website? Look no further than
+                                PeppuDraw! With our intuitive drag-and-drop interface and a wide range of customizable
+                                templates, creating a beautiful and functional website has never been easier.
+                            </font>
+                        </span>
+                    </div>
+                    <div id="iulsma">
+                        <span id="iaqzde">
+                            <font face="verdana, sans-serif">
+                                <br />
+                            </font>
+                        </span>
+                    </div>
+                    <div id="ic11hs">
+                        <span id="i6eiss">
+                            <font face="verdana, sans-serif">Simply choose the template that best suits your needs, and
+                                customize it to make it your own. With PeppuDraw, you can create a website that perfectly
+                                reflects your brand or personality in minutes.
+                            </font>
+                        </span>
+                    </div>
+                    </p>
+                    <button type="button" class="sub-button">Get Started</button>
+                </div>
+                <div>
+                    <footer>
+                        <div class="footer-content">
+                            <h3 id="i5l4t9">Build with PeppuDraw
+                            </h3>
+                            <p id="i56doj">PeppuDraw is creating a better user interface and experience for 'drag-n-drop'.
+                                With our intuitive interface and GrapesJs API, anyone can create a professional-looking
+                                website in a matter of minutes. Wether you're a small business owner, blogger, or creative
+                                professional, a drag and drop website builder can help you achieve that. 
+                            </p>
+                            <div class="socials">
+                                <span class="badge-link">ln</span>
+                                <span class="badge-link">f</span>
+                                <span class="badge-link">g</span>
+                                <span class="badge-link">in</span>
+                            </div>
+                        </div>
+                        <div class="footer-bottom">
+                            <p id="i3p4ne">copyright ©
+                                <a href="#">Made with Peppudraw</a>
+                            </p>
+                        </div>
+                    </footer>
+                </div>
+            </section-one>
+        </cards>
+        <cards>
+        </cards>
+        <section-one>
+        </section-one>
+        <section-two>
+        </section-two>
+    </body>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+
+    mounted() {
+        var items = document.querySelectorAll('#iksl');
+        for (var i = 0, len = items.length; i < len; i++) {
+            (function () {
+                var n3, t3 = this, e3 = "gjs-collapse", a3 = "max-height", o3 = 0, i3 = function () {
+                    var n4 = document.createElement("void"), t4 = {
+                        transition: "transitionend", OTransition: "oTransitionEnd", MozTransition: "transitionend", WebkitTransition: "webkitTransitionEnd"
+                    };
+                    for (var e4 in t4)
+                        if (void 0 !== n4.style[e4])
+                            return t4[e4];
+                }
+                    (), r2 = function (n4) {
+                        o3 = 1;
+                        var t4 = function (n5) {
+                            var t5 = window.getComputedStyle(n5), e5 = t5.display, o4 = parseInt(t5[a3]);
+                            if ("none" !== e5 && 0 !== o4)
+                                return n5.offsetHeight;
+                            n5.style.height = "auto", n5.style.display = "block", n5.style.position = "absolute", n5.style.visibility = "hidden";
+                            var i4 = n5.offsetHeight;
+                            return n5.style.height = "", n5.style.display = "", n5.style.position = "", n5.style.visibility = "", i4;
+                        }
+                            (n4), e4 = n4.style;
+                        e4.display = "block", e4.transition = "".concat(a3, " 0.25s ease-in-out"), e4.overflowY = "hidden", "" == e4[a3] && (e4[a3] = 0), 0 == parseInt(e4[a3]) ? (e4[a3] = "0", setTimeout(function () {
+                            e4[a3] = t4 + "px";
+                        }
+                            , 10)) : e4[a3] = "0";
+                    };
+                e3 in t3 || t3.addEventListener("click", function (e4) {
+                    if (e4.preventDefault(), !o3) {
+                        var l2 = t3.closest("[data-gjs=navbar]"), c2 = null == l2 ? void 0 : l2.querySelector("[data-gjs=navbar-items]");
+                        c2 && r2(c2), n3 || (null == c2 || c2.addEventListener(i3, function () {
+                            o3 = 0;
+                            var n4 = c2.style;
+                            0 == parseInt(n4[a3]) && (n4.display = "", n4[a3] = "");
+                        }
+                        ), n3 = 1);
+                    }
+                }
+                ), t3[e3] = 1;
+            }
+                .bind(items[i]))();
+        }
+    }
+}
+</script>
