82e571f377b709126ce055610c3bf64787c2f975
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-28 11:40:00 +0100

    successfully created navbar component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,5 @@
 <template>
     <body id="iljx">
-        <div class="navbar">
-            <div class="navbar-container" data-gjs="navbar">
-                <a href="/" class="navbar-brand"><img src="../assets/logo.png" class="nav-img"/></a>
-                <div class="navbar-burger" id="iksl">
-                    <div class="navbar-burger-line">
-                    </div>
-                    <div class="navbar-burger-line">
-                    </div>
-                    <div class="navbar-burger-line">
-                    </div>
-                </div>
-                <div data-gjs="navbar-items" class="navbar-items-c">
-                    <nav class="navbar-menu">
-                        <a href='/' class="navbar-menu-link">Home</a>
-                        <a href='/about' class="navbar-menu-link">About</a>
-                        <a href='/contact' class="navbar-menu-link">Contact</a>
-                    </nav>
-                </div>
-            </div>
-        </div>
         <hero>
             <div class="contain">
                 <div class="text">
@@ -301,89 +281,8 @@ export default {
 body {
   margin: 0;
 }
-.navbar{
-  background-color:rgb(17, 17, 17);
-  color:rgb(221, 221, 221);
-  min-height:50px;
-  width:100%;
-}
-.navbar-container{
-  max-width:950px;
-  margin-top:0px;
-  margin-right:auto;
-  margin-bottom:0px;
-  margin-left:auto;
-  width:95%;
-}
-.navbar-items-c{
-  display:inline-block;
-  float:right;
-}
-.navbar-container::after{
-  content:"";
-  clear:both;
-  display:block;
-}
-.navbar-brand{
-  padding-top:10px;
-  padding-bottom:10px;
-  display:block;
-  float:left;
-}
 .nav-img {
     width: 10vw;
-}
-.navbar-menu{
-  padding-top:10px;
-  padding-right:0px;
-  padding-bottom:10px;
-  padding-left:0px;
-  display:block;
-  float:right;
-  margin-top:0px;
-  margin-right:0px;
-  margin-bottom:0px;
-  margin-left:0px;
-}
-.navbar-menu-link{
-  margin-top:0px;
-  margin-right:0px;
-  margin-bottom:0px;
-  margin-left:0px;
-  color:inherit;
-  text-decoration:none;
-  display:inline-block;
-  padding-top:10px;
-  padding-right:15px;
-  padding-bottom:10px;
-  padding-left:15px;
-  font-family:Helvetica, sans-serif;
-  font-size: medium;
-}
-.navbar-burger{
-  margin-top:10px;
-  margin-right:0px;
-  margin-bottom:10px;
-  margin-left:0px;
-  width:45px;
-  padding-top:5px;
-  padding-right:10px;
-  padding-bottom:5px;
-  padding-left:10px;
-  display:none;
-  float:right;
-  cursor:pointer;
-}
-.navbar-burger-line{
-  padding-top:1px;
-  padding-right:1px;
-  padding-bottom:1px;
-  padding-left:1px;
-  background-color:white;
-  margin-top:5px;
-  margin-right:0px;
-  margin-bottom:5px;
-  margin-left:0px;
 }
 .background {
     background-color: #ffffff;
@@ -738,21 +637,6 @@ body {
     padding-right:20px;
     padding-bottom:20px;
     padding-left:20px;
-  }
-}
-@media (max-width: 768px){
-  .navbar-items-c{
-    display:none;
-    width:100%;
-  }
-  .navbar-burger{
-    display:block;
-  }
-  .navbar-menu{
-    width:100%;
-  }
-  .navbar-menu-link{
-    display:block;
   }
 }
 </style>
