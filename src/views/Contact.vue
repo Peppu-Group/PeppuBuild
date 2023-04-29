f1dd246672e0336e941659579b483c35ecb9c1d7
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-29 18:25:06 +0100

    successfully added header and footer

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,14 @@
-<template></template>
+<template>
+<Headers/>
+<Footers/>
+</template>
   
 <script>
+import Footers from '../components/Footers.vue'
+import Headers from '../components/Headers.vue'
+
 export default {
     name: 'Contact',
+    components: {Headers, Footers}
 }
 </script>
