b1fa6876af93805b89aa5042337d904a10967803
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-04-29 18:22:36 +0100

    bootstraping contact ready

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import Callback from '../views/Callback.vue'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
     }
     /* 
     {
