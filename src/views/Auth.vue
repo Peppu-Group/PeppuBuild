01b6f4533a7bdc0f8b54ac96fc2bda27db851cc3
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-05-01 09:18:18 +0100

    added handle login

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -20,7 +20,6 @@
                 <div id="g_id_onload" data-client_id="913987535189-inmbarcfp0be3l5mhqcu5ca46ss8po7c.apps.googleusercontent.com" data-callback="handleCredentialResponse">
                 </div>
                 <div class="g_id_signin" data-type="standard" data-theme="filled_blue"></div>
-                <div class="g_id_signout" id="signout_button"><button>Sign Out</button></div>
             </div>
             <div class="logo-img">
                 <img id="iix4i" src="../assets/intro.png" />
@@ -36,9 +35,27 @@ export default {
     name: 'Auth',
 
     mounted() {
-        const button = document.getElementById('signout_button');
-    button.onclick = () => {
-      google.accounts.id.disableAutoSelect();
+        function decodeJwtResponse(token) {
+        let base64Url = token.split('.')[1]
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload)
+    }
+
+    let responsePayload;
+    function handleCredentialResponse(response) {
+        // decodeJwtResponse() is a custom function defined by you
+        // to decode the credential response.
+        responsePayload = decodeJwtResponse(response.credential);
+
+        console.log("ID: " + responsePayload.sub);
+        console.log('Full Name: ' + responsePayload.name);
+        console.log('Given Name: ' + responsePayload.given_name);
+        console.log('Family Name: ' + responsePayload.family_name);
+        console.log("Image URL: " + responsePayload.picture);
+        console.log("Email: " + responsePayload.email);
     }
     },
     methods: {
