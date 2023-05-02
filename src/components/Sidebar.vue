9ae724dd36d021ed6dbabb978b700fb7d4193b83
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-05-02 16:16:01 +0100

    dashboard ready

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -14,31 +14,19 @@
         <ul>
           <li>
             <a href="#" class="active"><span class="icon"><i class="fas fa-home">
-              </i></span><span class="item">Home</span></a>
+              </i></span><span class="item">Dashboard</span></a>
           </li>
           <li>
             <a href="#"><span class="icon"><i class="fas fa-desktop">
-              </i></span><span class="item">My Dashboard</span></a>
-          </li>
-          <li>
-            <a href="#"><span class="icon"><i class="fas fa-user-friends">
-              </i></span><span class="item">People</span></a>
+              </i></span><span class="item">Workspace</span></a>
           </li>
           <li>
             <a href="#"><span class="icon"><i class="fas fa-tachometer-alt">
-              </i></span><span class="item">Perfomance</span></a>
+              </i></span><span class="item">Notifications</span></a>
           </li>
           <li>
             <a href="#"><span class="icon"><i class="fas fa-database">
-              </i></span><span class="item">Development</span></a>
-          </li>
-          <li>
-            <a href="#"><span class="icon"><i class="fas fa-chart-line">
-              </i></span><span class="item">Reports</span></a>
-          </li>
-          <li>
-            <a href="#"><span class="icon"><i class="fas fa-user-shield">
-              </i></span><span class="item">Admin</span></a>
+              </i></span><span class="item">Help</span></a>
           </li>
           <li>
             <a href="#"><span class="icon"><i class="fas fa-cog">
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,90 @@
 <template>
-<Sidebar/>
+    <body id="if7l">
+        <Sidebar />
+        <div class="dash-content">
+            <div id="i2sw">My Dashboard
+            </div>
+            <section-one>
+                <div class="one-container">
+                    <div class="one-child">
+                        <p class="i0sf">
+                            Hi Anamika, welcome back!
+                        </p>
+                        <p class="i0sg">
+                            We hope you're having a great day and are 
+                            ready to dive back and start creating magic.
+                        </p>
+                    </div>
+                </div>
+                <div id="inyx">Projects
+                </div>
+            </section-one>
+        </div>
+    </body>
 </template>
 
 <script>
 import Sidebar from '../components/Sidebar.vue';
 
 export default {
-  name: 'Dashboard',
-  components: { Sidebar },
+    name: 'Dashboard',
+    components: { Sidebar },
 }
 </script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+.dash-content {
+    width: calc(100% - 225px);
+    margin-left: 225px;
+    transition: all 0.5s ease;
+}
+
+.one-container {
+    display: block;
+    width: 100%;
+    padding-top: 20px;
+    padding-right: 20px;
+    padding-bottom: 20px;
+    padding-left: 20px;
+}
+
+.one-child {
+    padding: 20px;
+    background-color: #d0edd8;
+    border-radius: 10px;
+}
+
+.i0sf {
+    font-size: 18px;
+    font-weight: 550;
+    padding-bottom: 20px;
+}
+
+#i2sw {
+    padding: 30px;
+    text-align: left;
+    font-family: Helvetica, sans-serif;
+    font-size: 25px;
+    font-weight: 500;
+}
+
+#inyx {
+    padding: 10px;
+    text-align: center;
+    font-family: Helvetica, sans-serif;
+}
+
+@media screen and (max-width: 800px) {
+    .one-container {
+        display: block;
+        width: 100%;
+        padding-top: 20px;
+        padding-right: 20px;
+        padding-bottom: 20px;
+        padding-left: 20px;
+    }
+}</style>
