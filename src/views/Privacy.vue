eb9ce2f4418a1e0ad7618a195c0a3ea8d38c9367
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-05-23 13:49:42 +0100

    final version of privacy page

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -9,8 +9,8 @@
                 <span id="ilg2">We take your privacy seriously. We do not share or sell your information with
                     anybody.</span>
             </section>
-            <section class="bdg-sect">
-                <h1 class="heading">Policy
+            <section class="p-content">
+                <h1 class="head-content">Policy
                 </h1>
                 <span id="ilg2">This Privacy Policy describes Our policies and procedures on the collection, use and
                     disclosure of Your information when You use the Service and tells You about Your privacy rights and how
@@ -23,16 +23,12 @@
                     collection and use of information in accordance with this policy. Unless otherwise defined in this
                     Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and
                     Conditions.</span>
-            </section>
-            <section class="bdg-sect">
-                <h1 class="heading">Methods of processing Data
+                <h1 class="head-content">Methods of processing Data
                 </h1>
-                <span id="ilg2">Peppubuild doesn't store user personal information or date. All data is stored in Firebase.
+                <span id="ilg2">Peppubuild doesn't store user personal information or data. All projects created are stored in Firebase.
                     <br /><br /><br /> Projects created and published by our users are stored in Firebase, we don't collect
-                    your personal information.</span>
-            </section>
-            <section class="bdg-sect">
-                <h1 class="heading">Changes to Policy
+                    your personal information, we only require your email address.</span>
+                <h1 class="head-content">Changes to Policy
                 </h1>
                 <span id="ilg2">This Privacy Policy is subject to occasional revision, and we reserve the right to modify
                     this Privacy Policy relating to Peppubuild at any time, effective upon posting of an updated version of
@@ -52,3 +48,19 @@ export default {
     components: { Footers, Headers },
 }
 </script>
+
+<style>
+.p-content {
+    padding-bottom: 50px;
+    text-align: justify;
+    padding-left: 10vw;
+    padding-right: 10vw;
+}
+
+.head-content {
+    margin: 0;
+    padding: 20px;
+    font-size: 30px;
+    text-align: center;
+}
+</style>
